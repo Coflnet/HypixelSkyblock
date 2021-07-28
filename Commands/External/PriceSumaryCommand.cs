80c0d707378a1622ced646b05b435471bb64c7cf
Äkwav <xekwavx@gmail.com>
2021-07-28 15:24:58 +0200

    added mode

--- Commands/External/PriceSumaryCommand.cs
+++ Commands/External/PriceSumaryCommand.cs
@@ -19,12 +19,14 @@ namespace hypixel
                 var minTime = DateTime.Now.Subtract(TimeSpan.FromDays(1));
                 var auctions = (await context.Auctions.Where(a => a.ItemId == id && a.End < DateTime.Now && a.End > minTime && a.HighestBidAmount > 0)
                                 .Select(a => a.HighestBidAmount).ToListAsync()).OrderByDescending(p => p).ToList();
-
+                var mode = auctions.GroupBy(a => a).OrderByDescending(a => a.Count()).FirstOrDefault();
                 var result = new Result()
                 {
                     Max = auctions.FirstOrDefault(),
                     Med = auctions.Count > 0 ? auctions.Skip(auctions.Count() / 2).FirstOrDefault() : 0,
                     Min = auctions.LastOrDefault(),
+                    Mean = auctions.Average(),
+                    Mode = mode.Key,
                     Volume = auctions.Count()
                 };
 
@@ -41,6 +43,10 @@ namespace hypixel
             public long Min;
             [DataMember(Name = "median")]
             public long Med;
+            [DataMember(Name = "mean")]
+            public double Mean;
+            [DataMember(Name = "mode")]
+            public long Mode;
             [DataMember(Name = "volume")]
             public long Volume;
         }
