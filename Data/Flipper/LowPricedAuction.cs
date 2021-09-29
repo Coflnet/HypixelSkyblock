16065c242bcf4ca88b865c55bc8ba9d802d58a4c
Äkwav <xekwavx@gmail.com>
2021-09-29 22:37:22 +0200

    flip base class

---
+++ Data/Flipper/LowPricedAuction.cs
@@ -0,0 +1,28 @@
+using System.Runtime.Serialization;
+using hypixel;
+
+namespace Coflnet.Sky
+{
+    [DataContract]
+    public class LowPricedAuction
+    {
+        [DataMember(Name = "target")]
+        public int TargetPrice;
+        [DataMember(Name = "vol")]
+        public float DailyVolume;
+        [DataMember(Name = "auc")]
+        public SaveAuction Auction;
+        [DataMember(Name = "finder")]
+        public FinderType Finder;
+
+        [IgnoreDataMember]
+        public long UId => AuctionService.Instance.GetId(this.Auction.Uuid);
+
+        public enum FinderType
+        {
+            UNKOWN,
+            FLIPPER,
+            SNIPER
+        }
+    }
+}
