5cb38a6a300dff25ad40090da096b2afeae01484
Äkwav <xekwavx@gmail.com>
2021-10-15 16:13:24 +0200

    add method to update player name

--- Server/Services/PlayerService.cs
+++ Server/Services/PlayerService.cs
@@ -19,5 +19,15 @@ namespace hypixel
                 return context.Players.Where(p => p.UuId == uuid).FirstOrDefaultAsync();
             }
         }
+        public async Task<Player> UpdatePlayerName(string uuid)
+        {
+            using (var context = new HypixelContext())
+            {
+                var player = await context.Players.Where(p => p.UuId == uuid).FirstOrDefaultAsync();
+                player.ChangedFlag = true;
+                await context.SaveChangesAsync();
+                return player;
+            }
+        }
     }
 }
