ca28fd85dabdbc2f535b1ed87e70f57d37afa401
Äkwav <xekwavx@gmail.com>
2022-01-09 13:08:33 +0100

    add special case for null ids

--- Server/Services/AuctionService.cs
+++ Server/Services/AuctionService.cs
@@ -33,6 +33,8 @@ namespace hypixel
 
         public long GetId(string uuid)
         {
+            if(uuid == null)
+                return -1;
             if (uuid.Length > 17)
                 uuid = uuid.Substring(0, 17);
             var builder = new System.Text.StringBuilder(uuid);
