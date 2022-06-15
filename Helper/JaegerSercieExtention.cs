ed6919218dc1cea06fbbe2ef6c9d532ec1022790
Äkwav <ekwav@coflnet.com>
2022-06-15 18:08:12 +0200

    use opentelemetry

--- Helper/JaegerSercieExtention.cs
+++ Helper/JaegerSercieExtention.cs
@@ -4,6 +4,12 @@ using Jaeger.Senders.Thrift;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Context.Propagation;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Shims.OpenTracing;
+using OpenTelemetry.Trace;
 using OpenTracing;
 using OpenTracing.Util;
 
@@ -13,22 +19,41 @@ namespace Coflnet.Sky.Core
     {
         public static void AddJaeger(this IServiceCollection services, double samplingRate = 0.03, double lowerBoundInSeconds = 30)
         {
+            services.AddOpenTelemetryTracing((builder) => builder
+                .AddAspNetCoreInstrumentation()
+                .AddHttpClientInstrumentation()
+                .AddSqlClientInstrumentation()
+                .AddJaegerExporter(j=>{
+                    j.Protocol = JaegerExportProtocol.HttpBinaryThrift;
+                })
+                .AddConsoleExporter()
+                .SetSampler(new TraceIdRatioBasedSampler(samplingRate))
+            );
+
             services.AddSingleton<ITracer>(serviceProvider =>
             {
                 ILoggerFactory loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
                 IConfiguration iConfiguration = serviceProvider.GetRequiredService<IConfiguration>();
-
+                /*
                 Jaeger.Configuration.SenderConfiguration.DefaultSenderResolver = new SenderResolver(loggerFactory)
                         .RegisterSenderFactory<ThriftSenderFactory>();
 
                 ISampler sampler = new GuaranteedThroughputSampler(samplingRate, lowerBoundInSeconds);
-                if(samplingRate >= 1)
+                if (samplingRate >= 1)
                     sampler = new ConstSampler(true);
                 var config = Jaeger.Configuration.FromIConfiguration(loggerFactory, iConfiguration);
 
                 ITracer tracer = config.GetTracerBuilder()
                     .WithSampler(sampler)
                     .Build();
+                */
+                var traceProvider = serviceProvider.GetRequiredService<TracerProvider>();
+
+                // Instantiate the OpenTracing shim. The underlying OpenTelemetry tracer will create
+                // spans using the "MyCompany.MyProduct.MyWebServer" source.
+                var tracer = new TracerShim(
+                    TracerProvider.Default.GetTracer("MyCompany.MyProduct.MyWebServer"),
+                    Propagators.DefaultTextMapPropagator);
 
                 try
                 {
@@ -42,7 +67,7 @@ namespace Coflnet.Sky.Core
 
                 return tracer;
             });
-            services.AddOpenTracing();
+            //services.AddOpenTracing();
         }
     }
 }
