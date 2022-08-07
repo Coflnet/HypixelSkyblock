305ed3a712bf205532693a07d57251d222af8537
Äkwav <ekwav@coflnet.com>
2022-08-07 10:45:48 +0200

    add consume_process_failed metric to kafka consume

--- Data/KafkaConsumer.cs
+++ Data/KafkaConsumer.cs
@@ -5,11 +5,14 @@ using System.Threading;
 using System.Threading.Tasks;
 using Confluent.Kafka;
 using Coflnet.Sky.Core;
+using Prometheus;
 
 namespace Coflnet.Kafka
 {
     public class KafkaConsumer
     {
+
+        static Counter processFail = Metrics.CreateCounter("consume_process_failed", "How often processing of consumed messages failed");
         /// <summary>
         /// Generic consumer
         /// </summary>
@@ -28,62 +31,21 @@ namespace Coflnet.Kafka
                                             AutoOffsetReset start = AutoOffsetReset.Earliest,
                                             IDeserializer<T> deserializer = null)
         {
-            try
-            {
-                var conf = new ConsumerConfig
+            while (!cancleToken.IsCancellationRequested)
+                try
                 {
-                    GroupId = groupId,
-                    BootstrapServers = host,
-                    // Note: The AutoOffsetReset property determines the start offset in the event
-                    // there are not yet any committed offsets for the consumer group for the
-                    // topic/partitions of interest. By default, offsets are committed
-                    // automatically, so in this example, consumption will only start from the
-                    // earliest message in the topic 'my-topic' the first time you run the program.
-                    AutoOffsetReset = start,
-                    AutoCommitIntervalMs = 0,
-                    EnableAutoCommit = false
-                };
-                if (deserializer == null)
-                    deserializer = SerializerFactory.GetDeserializer<T>();
-
-                using (var c = new ConsumerBuilder<Ignore, T>(conf).SetValueDeserializer(deserializer).Build())
-                {
-                    c.Subscribe(topic);
-                    try
-                    {
-                        // free the calling task
-                        await Task.Yield();
-                        while (!cancleToken.IsCancellationRequested)
-                        {
-                            try
-                            {
-                                var cr = c.Consume(cancleToken);
-                                if (cr == null)
-                                    continue;
-
-
-                                await action(cr.Message.Value);
-
-                                c.Commit(new TopicPartitionOffset[] { cr.TopicPartitionOffset });
-                            }
-                            catch (ConsumeException e)
-                            {
-                                dev.Logger.Instance.Error(e, $"Kafka consumer process for {topic}");
-                            }
-                        }
-                    }
-                    catch (OperationCanceledException)
-                    {
-                        // Ensure the consumer leaves the group cleanly and final offsets are committed.
-                        c.Close();
-                    }
+                    await ConsumeBatch(host, topic, async batch => {
+                        foreach (var message in batch)
+                            await action(message);
+                    }, cancleToken, groupId, 1, start, deserializer);
                 }
-            }
-            catch (Exception e)
-            {
-                dev.Logger.Instance.Error(e, $"Kafka consumer process for {topic}");
-            }
+                catch (Exception e)
+                {
+                    dev.Logger.Instance.Error(e, $"Kafka consumer process for {topic}");
+                    processFail.Inc();
+                }
         }
+
 
         /// <summary>
         /// Consume a batch of messages for a single topic
@@ -95,15 +57,17 @@ namespace Coflnet.Kafka
         /// <param name="groupId"></param>
         /// <param name="maxChunkSize"></param>
         /// <param name="start"></param>
+        /// <param name="deserializer"></param>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
         public static Task ConsumeBatch<T>(string host, string topic, Func<IEnumerable<T>, Task> action,
                                             CancellationToken cancleToken,
                                             string groupId = "default",
                                             int maxChunkSize = 500,
-                                            AutoOffsetReset start = AutoOffsetReset.Earliest)
+                                            AutoOffsetReset start = AutoOffsetReset.Earliest,
+                                            IDeserializer<T> deserializer = null)
         {
-            return ConsumeBatch<T>(host, new string[] { topic }, action, cancleToken, groupId, maxChunkSize, start);
+            return ConsumeBatch<T>(host, new string[] { topic }, action, cancleToken, groupId, maxChunkSize, start, deserializer);
         }
 
         /// <summary>
@@ -116,32 +80,37 @@ namespace Coflnet.Kafka
         /// <param name="groupId"></param>
         /// <param name="maxChunkSize"></param>
         /// <param name="start"></param>
+        /// <param name="deserializer"></param>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
         public static async Task ConsumeBatch<T>(string host, string[] topics, Func<IEnumerable<T>, Task> action,
                                             CancellationToken cancleToken,
                                             string groupId = "default",
                                             int maxChunkSize = 500,
-                                            AutoOffsetReset start = AutoOffsetReset.Earliest)
+                                            AutoOffsetReset start = AutoOffsetReset.Earliest,
+                                            IDeserializer<T> deserializer = null)
         {
             var batch = new Queue<ConsumeResult<Ignore, T>>();
             var conf = new ConsumerConfig
             {
                 GroupId = groupId,
-                BootstrapServers = Program.KafkaHost,
+                BootstrapServers = host,
                 // Note: The AutoOffsetReset property determines the start offset in the event
                 // there are not yet any committed offsets for the consumer group for the
                 // topic/partitions of interest. By default, offsets are committed
                 // automatically, so in this example, consumption will only start from the
                 // earliest message in the topic 'my-topic' the first time you run the program.
-                AutoOffsetReset = AutoOffsetReset.Earliest,
-                AutoCommitIntervalMs = 0, 
+                AutoOffsetReset = start,
+                AutoCommitIntervalMs = 0,
                 EnableAutoCommit = false
             };
             // in case this method is awaited on in a backgroundWorker
             await Task.Yield();
 
-            using (var c = new ConsumerBuilder<Ignore, T>(conf).SetValueDeserializer(SerializerFactory.GetDeserializer<T>()).Build())
+            if (deserializer == null)
+                deserializer = SerializerFactory.GetDeserializer<T>();
+
+            using (var c = new ConsumerBuilder<Ignore, T>(conf).SetValueDeserializer(deserializer).Build())
             {
                 c.Subscribe(topics);
                 try
