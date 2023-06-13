d982be0a5c277c44d4692359ca21b8636062b652
Äkwav <ekwav@coflnet.com>
2023-06-13 20:44:05 +0200

    fire aspect 3 is worth something

--- Data/Flipper/Constants.cs
+++ Data/Flipper/Constants.cs
@@ -98,6 +98,7 @@ namespace Coflnet.Sky.Core
             new Enchantment(Enchantment.EnchantmentType.prosperity,1),
             new Enchantment(Enchantment.EnchantmentType.dedication,1),
             new Enchantment(Enchantment.EnchantmentType.tabasco,3),
+            new Enchantment(Enchantment.EnchantmentType.fire_aspect,3),
         };
 
         /// <summary>
