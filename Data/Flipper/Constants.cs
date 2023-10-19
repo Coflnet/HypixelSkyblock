bcbaf5840cf7b88e672d1e452b7f2bcc3b450e59
Äkwav <ekwav@coflnet.com>
2023-10-19 22:44:15 +0200

    drop true protection from relevant enchants

--- Data/Flipper/Constants.cs
+++ Data/Flipper/Constants.cs
@@ -56,7 +56,7 @@ namespace Coflnet.Sky.Core
             new Enchantment(Enchantment.EnchantmentType.snipe,4),
             new Enchantment(Enchantment.EnchantmentType.pristine,2), // maybe 1 as well
             new Enchantment(Enchantment.EnchantmentType.overload,2),
-            new Enchantment(Enchantment.EnchantmentType.true_protection,1),
+            //new Enchantment(Enchantment.EnchantmentType.true_protection,1), cost alsmot 1m but is mostly useless
             new Enchantment(Enchantment.EnchantmentType.smite,7),
             new Enchantment(Enchantment.EnchantmentType.critical,8),
             new Enchantment(Enchantment.EnchantmentType.giant_killer,7),
